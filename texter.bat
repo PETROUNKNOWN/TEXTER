76a83d902e838326a2bbba444d765cc253fbb2e5
peet <petrounknown@gmail.com>
2025-01-16 17:01:27 +0300

    Updated Code

--- texter.bat
+++ texter.bat
@@ -1,6 +1,2 @@
 TIMEOUT /T 7
-
-echo >script.vbs set shell = CreateObject("WScript.Shell"):shell.SendKeys "enter the text you want typed here like this" & script.vbs
-
-
-
+echo >script.vbs set shell = CreateObject("WScript.Shell"):shell.SendKeys "<YOUR TEXT HERE>" & script.vbs
